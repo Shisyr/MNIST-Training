686b137f492e9a61b5cf7885c47dfea6d141e9d7
turdubars <turdubaev.arstan@gmail.com>
2018-10-01 22:42:18 +0600

    Added problem1 and problem2

--- problem1.c
+++ problem1.c
@@ -6,9 +6,10 @@
 #define numInputNodes 785
 #define numOutputNodes 10
 
-int initializeTarget(float target[], int numberOnPicture);
+void initializeTarget(float target[], int numberOnPicture);
 void createRandomWeightMatrix(float weightMatrix[][numInputNodes]);
-void getSquashedOutput(float output[], int inputVec[], float weightMatrix[][numInputNodes]);
+void getOutput(float output[], int inputVec[], float weightMatrix[][numInputNodes]);
+void squashOutput(float output[]) ;
 void getError(float error[], float target[], float output[]);
 float getAverageError(float error[]);
 void updateWights(float weightMatrix[][numInputNodes], int inputVec[], float error[], float learningRate);
@@ -37,13 +38,11 @@ int main(int argc, char const *argv[]) {
   }
 
   int inputVec[numInputNodes]; // creating variable to store input values
-  int pictureIndex = 2;
+  // int pictureIndex = 0;
 
-  float learningRate = 0.1;
+  float learningRate = 0.5;
   // number of training loops
-  int trainLoopNum = 20;
-
-  float weightMatrix[numOutputNodes][numInputNodes];
+  // int trainLoopNum = 20;
 
   float target[numOutputNodes];
 
@@ -58,43 +57,73 @@ int main(int argc, char const *argv[]) {
   // 4 = 6
   // ...
 
-  for (int i = 0; i < 6; i++) {
+  float weightMatrix[numOutputNodes][numInputNodes];
+  createRandomWeightMatrix(weightMatrix);
 
-    printf("\n\nImage noise = %.02f\n\n", sampNoise);
+  // float averageError;
 
-    // loadType = 0, 60k training images
-    // loadType = 1, 10k testing images
-    // loadType = 2, 10 specific images from training set
-    // printf("number of training patterns = %d\n", sizeData);
+  // // load MNIST picture and convert it to a vector
+  // get_input(inputVec, zData, pictureIndex, sampNoise);
+  //
+  // // draw the picture with specified label
+  // draw_input(inputVec, zData[pictureIndex].label);
 
-    // inspect the training samples we will work with:
-    // for(int pictureIndex = 0; pictureIndex < sizeData; pictureIndex++){
 
-      get_input(inputVec, zData, pictureIndex, sampNoise);
-      draw_input(inputVec, zData[pictureIndex].label);
+  // loadType = 0, 60k training images
+  // loadType = 1, 10k testing images
+  // loadType = 2, 10 specific images from training set
+  // printf("number of training patterns = %d\n", sizeData);
+for(float noise = 0.0; noise < 0.6; noise += 0.1) {
 
-      createRandomWeightMatrix(weightMatrix);
+  // printf("sampNoise = %.02f\n", noise);
 
-      int whichNumber = initializeTarget(target, zData[pictureIndex].label);
+  createRandomWeightMatrix(weightMatrix);
 
-      for (int i = 1; i <= trainLoopNum; i++) {
+  for(int simulation = 0; simulation < 20; simulation++) {
 
-        printf("\nTraining loop #%d for pictureIndex #%d\n\n", i, pictureIndex);
+    // printf("\nSimulation #%d\n\n", simulation);
+    float theError = 0;
+    for(int pictureIndex = 0; pictureIndex < sizeData; pictureIndex++){
+        get_input(inputVec, zData, pictureIndex, noise);
+        // draw_input(inputVec, zData[pictureIndex].label);
 
-        getSquashedOutput(output, inputVec, weightMatrix);
+        initializeTarget(target, zData[pictureIndex].label);
+          // printf("\nTraining loop #%d for pictureIndex #%d\n\n", i, pictureIndex);
+
+        getOutput(output, inputVec, weightMatrix);
+
+        squashOutput(output);
 
         getError(error, target, output);
-
-        // printf("average Error = %f\n", getAverageError(error));
-
-        printf("\ntargetted error[%d] = %f\n", whichNumber, error[whichNumber]);
+        //
+        // printf("%f ", getAverageError(error));
+        //
+        theError += getAverageError(error);
 
         updateWights(weightMatrix, inputVec, error, learningRate);
-      }
-
-      sampNoise += 0.1;
+    }
+    printf("%f, ", (theError / sizeData));
   }
 
+  printf("\n");
+
+}
+
+
+
+
+
+
+  // int outPic = 6;
+  // get_input(inputVec, zData, outPic, 0);
+  // draw_input(inputVec, zData[outPic].label);
+  //
+  // getOutput(output, inputVec, weightMatrix);
+  //
+  // squashOutput(output);
+  //
+  // for(int i = 0; i < 10; i++) {
+  //   printf("squashedOutput[%d] = %f\n", i, output[i]);
   // }
 
   return 0;
@@ -113,51 +142,56 @@ void createRandomWeightMatrix(float weightMatrix[][numInputNodes]) {
         weightMatrix[i][j] = rand_weight();
       }
     }
-
-    printf("Created random weigths\n\n");
   }
 
 
-int initializeTarget(float target[], int numberOnPicture) {
+void initializeTarget(float target[], int numberOnPicture) {
 
-  int targetedNumber = 0;
   for(int i = 0; i < numOutputNodes; i++) {
     if (i == numberOnPicture) {
       target[i] = 1;
-      targetedNumber = i;
     } else {
       target[i] = 0;
     }
-    printf("target[%d] = %f\n", i, target[i]);
+    // printf("target[%d] = %f\n", i, target[i]);
   }
-
-  return targetedNumber;
 }
 
 // calculate output by matrix multiplication of inputs and weights
 // and then squash output
-void getSquashedOutput(float output[], int inputVec[], float weightMatrix[][numInputNodes]) {
+void getOutput(float output[], int inputVec[], float weightMatrix[][numInputNodes]) {
 
   // variable to store results of matrix multiplication
-  float multiplicationResult = 0;
+  float sumMultiplicationResult;
 
-  for (int i = 0; i < numOutputNodes; i++) {
-    for (int j = 0; j < numInputNodes; j++) {
-      multiplicationResult += weightMatrix[i][j] * inputVec[j];
+  for(int i = 0; i < numOutputNodes; i++) {
+    sumMultiplicationResult = 0;
+    for(int j = 0; j < numInputNodes; j++) {
+
+      float multiplicationResult = inputVec[j] * weightMatrix[i][j];
+      sumMultiplicationResult += multiplicationResult;
     }
-    output[i] = multiplicationResult;
-    output[i] = 1.0 / (1.0 + pow(M_E, -1 * output[i]));
-    printf("squashed output[%d] = %f\n", i, output[i]);
 
-    multiplicationResult = 0;
+    output[i] = sumMultiplicationResult;
+    // printf("output[%d] = %f\n", i, sumMultiplicationResult);
   }
 }
+
+void squashOutput(float output[]) {
+
+  for(int i = 0; i < numOutputNodes; i++) {
+    output[i] = 1.0 / (1.0 + pow(M_E, -1 * output[i]));
+    // printf("squashed output[%d] = %f\n", i, output[i]);
+  }
+}
+
 
 // get error
 void getError(float error[], float target[], float output[]) {
 
   for (int i = 0; i < numOutputNodes; i++) {
     error[i] = target[i] - output[i];
+
     // printf("error[%d] = %f\n", i, error[i]);
   }
 }
@@ -166,10 +200,10 @@ float getAverageError(float error[]) {
 
   float allErrorSum = 0;
   for (int i = 0; i < numOutputNodes; i++) {
-    allErrorSum += error[i];
+    allErrorSum += fabs(error[i]);
   }
 
-  return (allErrorSum/numOutputNodes);
+  return (allErrorSum / numOutputNodes);
 }
 
 // calculate deltaWeight
@@ -180,7 +214,9 @@ void updateWights(float weightMatrix[][numInputNodes], int inputVec[], float err
   for(int i = 0; i < numOutputNodes; i++) {
     for (int j = 0; j < numInputNodes; j++) {
       deltaWeightMatrix[i][j] = inputVec[j] * error[i] * learningRate;
+      // float temp = weightMatrix[i][j];
       weightMatrix[i][j] += deltaWeightMatrix[i][j];
+
     }
   }
 }
--- problem2.c
+++ problem2.c
@@ -8,7 +8,8 @@
 
 void initializeTarget(float target[], int numberOnPicture);
 void createRandomWeightMatrix(float weightMatrix[][numInputNodes]);
-void getSquashedOutput(float output[], int inputVec[], float weightMatrix[][numInputNodes]);
+void getOutput(float output[], int inputVec[], float weightMatrix[][numInputNodes]);
+void squashOutput(float output[]) ;
 void getError(float error[], float target[], float output[]);
 float getAverageError(float error[]);
 void updateWights(float weightMatrix[][numInputNodes], int inputVec[], float error[], float learningRate);
@@ -30,18 +31,27 @@ int main(int argc, char const *argv[]) {
 
   mnist_data *zData;      // each image is 28x28 pixels
   unsigned int sizeData;  // depends on loadType
-  int loadType = 2; // loadType may be: 0, 1, or 2
+  int loadType = 0; // loadType may be: 0, 1, or 2
   if (mnistLoad(&zData, &sizeData, loadType)){
       printf("something went wrong loading data set\n");
       return -1;
   }
 
-  int inputVec[numInputNodes]; // creating variable to store input values
-  int pictureIndex = 1;
+  mnist_data *zData1;      // each image is 28x28 pixels
+  unsigned int sizeData1;  // depends on loadType
+  int loadType1 = 1; // loadType may be: 0, 1, or 2
+  if (mnistLoad(&zData1, &sizeData1, loadType1)){
+      printf("something went wrong loading data set\n");
+      return -1;
+  }
 
-  float learningRate = 0.05;
+
+  int inputVec[numInputNodes]; // creating variable to store input values
+  // int pictureIndex = 0;
+
+  // float learningRate = 0.5;
   // number of training loops
-  int trainLoopNum = 10;
+  // int trainLoopNum = 20;
 
   float target[numOutputNodes];
 
@@ -71,28 +81,81 @@ int main(int argc, char const *argv[]) {
   // loadType = 0, 60k training images
   // loadType = 1, 10k testing images
   // loadType = 2, 10 specific images from training set
-  printf("number of training patterns = %d\n", sizeData);
+  // printf("number of training patterns = %d\n", sizeData);
 
-  // inspect the training samples we will work with:
-  for(int pictureIndex = 0; pictureIndex < sizeData; pictureIndex++){
-      get_input(inputVec, zData, pictureIndex, sampNoise);
-      draw_input(inputVec, zData[pictureIndex].label);
+// for(float learningRate = 0.1; learningRate >= 0.01; learningRate /= 10) {
+for(float learningRate = 0.1; learningRate >= 0.00001; learningRate /= 10) {
 
-      initializeTarget(target, zData[pictureIndex].label);
+  // printf("sampNoise = %.02f\n", noise);
 
-      for (int i = 1; i <= trainLoopNum; i++) {
+  createRandomWeightMatrix(weightMatrix);
 
-        printf("\nTraining loop #%d for pictureIndex #%d\n\n", i, pictureIndex);
+  for(int simulation = 0; simulation < 5; simulation++) {
 
-        getSquashedOutput(output, inputVec, weightMatrix);
+    // printf("\nSimulation #%d\n\n", simulation);
+    // float theError = 0;
+    for(int pictureIndex = 0; pictureIndex < sizeData; pictureIndex++){
+        get_input(inputVec, zData, pictureIndex, sampNoise);
+        // draw_input(inputVec, zData[pictureIndex].label);
+
+        initializeTarget(target, zData[pictureIndex].label);
+          // printf("\nTraining loop #%d for pictureIndex #%d\n\n", i, pictureIndex);
+
+        getOutput(output, inputVec, weightMatrix);
+
+        squashOutput(output);
 
         getError(error, target, output);
-
-        printf("average Error = %f\n", getAverageError(error));
+        //
+        // printf("%f ", getAverageError(error));
+        //
+        // theError += getAverageError(error);
 
         updateWights(weightMatrix, inputVec, error, learningRate);
-      }
+    }
+    // printf("%f, ", (theError / sizeData));
+    float theError = 0;
+
+    for(int testImage = 0; testImage < sizeData1; testImage++) {
+
+      get_input(inputVec, zData, testImage, sampNoise);
+      // draw_input(inputVec, zData[testImage].label);
+
+      getOutput(output, inputVec, weightMatrix);
+
+      squashOutput(output);
+      //
+      // for(int outIndex = 0; outIndex < 10; outIndex++) {
+      //   printf("squashedOutput[%d] = %f\n", outIndex, output[outIndex]);
+      // }
+
+      getError(error, target, output);
+
+      theError += getAverageError(error);
+
+    }
+    printf("%f, ", (theError / sizeData1));
   }
+  printf("\n");
+
+
+}
+
+
+
+
+
+//   int outPic = 6;
+//   get_input(inputVec, zData, outPic, 0);
+//   draw_input(inputVec, zData[outPic].label);
+//
+//   getOutput(output, inputVec, weightMatrix);
+//
+//   squashOutput(output);
+//
+//   for(int i = 0; i < 10; i++) {
+//     printf("squashedOutput[%d] = %f\n", i, output[i]);
+//   }
 
   return 0;
 
@@ -121,35 +184,46 @@ void initializeTarget(float target[], int numberOnPicture) {
     } else {
       target[i] = 0;
     }
-    printf("target[%d] = %f\n", i, target[i]);
+    // printf("target[%d] = %f\n", i, target[i]);
   }
 }
 
 // calculate output by matrix multiplication of inputs and weights
 // and then squash output
-void getSquashedOutput(float output[], int inputVec[], float weightMatrix[][numInputNodes]) {
+void getOutput(float output[], int inputVec[], float weightMatrix[][numInputNodes]) {
 
   // variable to store results of matrix multiplication
-  float multiplicationResult = 0;
+  float sumMultiplicationResult;
 
-  for (int i = 0; i < numOutputNodes; i++) {
-    for (int j = 0; j < numInputNodes; j++) {
-      multiplicationResult += weightMatrix[i][j] * inputVec[j];
+  for(int i = 0; i < numOutputNodes; i++) {
+    sumMultiplicationResult = 0;
+    for(int j = 0; j < numInputNodes; j++) {
+
+      float multiplicationResult = inputVec[j] * weightMatrix[i][j];
+      sumMultiplicationResult += multiplicationResult;
     }
-    output[i] = multiplicationResult;
-    output[i] = 1.0 / (1.0 + pow(M_E, -1 * output[i]));
-    printf("squashed output[%d] = %f\n", i, output[i]);
 
+    output[i] = sumMultiplicationResult;
+    // printf("output[%d] = %f\n", i, sumMultiplicationResult);
   }
-  multiplicationResult = 0;
 }
+
+void squashOutput(float output[]) {
+
+  for(int i = 0; i < numOutputNodes; i++) {
+    output[i] = 1.0 / (1.0 + pow(M_E, -1 * output[i]));
+    // printf("squashed output[%d] = %f\n", i, output[i]);
+  }
+}
+
 
 // get error
 void getError(float error[], float target[], float output[]) {
 
   for (int i = 0; i < numOutputNodes; i++) {
     error[i] = target[i] - output[i];
-    printf("error[%d] = %f\n", i, error[i]);
+
+    // printf("error[%d] = %f\n", i, error[i]);
   }
 }
 
@@ -157,10 +231,10 @@ float getAverageError(float error[]) {
 
   float allErrorSum = 0;
   for (int i = 0; i < numOutputNodes; i++) {
-    allErrorSum += error[i];
+    allErrorSum += fabs(error[i]);
   }
 
-  return (allErrorSum/numOutputNodes);
+  return (allErrorSum / numOutputNodes);
 }
 
 // calculate deltaWeight
@@ -171,7 +245,9 @@ void updateWights(float weightMatrix[][numInputNodes], int inputVec[], float err
   for(int i = 0; i < numOutputNodes; i++) {
     for (int j = 0; j < numInputNodes; j++) {
       deltaWeightMatrix[i][j] = inputVec[j] * error[i] * learningRate;
+      // float temp = weightMatrix[i][j];
       weightMatrix[i][j] += deltaWeightMatrix[i][j];
+
     }
   }
 }
